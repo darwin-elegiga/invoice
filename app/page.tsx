4947c2551b758c009ee73178f2c51bc9a81529f2
Darwin Alejandro <darwin.elegiga02@gmail.com>
2025-05-30 07:27:48 -0400

    x

--- app/page.tsx
+++ app/page.tsx
@@ -251,7 +251,7 @@ const exportToPDFLightweight = () => {
   // Total
   pdf.setFontSize(12)
   pdf.text(`TOTAL: ${calculateTotal()} EUR`, pageWidth - margin, yPos, { align: 'right' })
-// 
+// s
   pdf.save(`FACTURA ${getCurrentMonthInvoiceNumber()} Darwin Alejandro.pdf`)
 }
 
